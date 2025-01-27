afaf3bcee876d7b4018d23b9ad2c2cd67539921a
nordwik <126633118+nordwik@users.noreply.github.com>
2025-01-27 12:03:22 +0100

    Database setup

--- Backend/database.py
+++ Backend/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
+from models import Base
 # SQLite database URL
 DATABASE_URL = "sqlite:///./Items.db"
 
@@ -9,3 +9,6 @@ engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
 # Session configuration
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Create tables in the database
+Base.metadata.create_all(bind=engine)
