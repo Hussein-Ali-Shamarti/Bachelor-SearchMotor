a48146219bc971c8ac8992e4de91e3bd20861175
nordwik <126633118+nordwik@users.noreply.github.com>
2025-01-29 09:26:39 +0100

    Revert "Databse setup and upload + datacollections files"
    This reverts commit b117bba6f921470a408af33378380f900daf2fd0.

--- Backend/database.py
+++ Backend/database.py
@@ -1,10 +1,6 @@
-"""
-This file is used to create a database engine and session configuration for the database.
-"""
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from models import Base
+
 # SQLite database URL
 DATABASE_URL = "sqlite:///./Items.db"
 
@@ -13,6 +9,3 @@ engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
 # Session configuration
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# Create tables in the database
-Base.metadata.create_all(bind=engine)
