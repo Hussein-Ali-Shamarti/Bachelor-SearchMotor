6da379be37dbafd0d573a3a5a75b4c39cfd60a1d
nordwik <126633118+nordwik@users.noreply.github.com>
2025-02-19 13:55:32 +0100

    Added a config which easily distributes DB URL in the system

--- Backend/database.py
+++ Backend/database.py
@@ -5,14 +5,18 @@ This file is used to create a database engine and session configuration for the 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import Base
+from config import DATABASE_URL
+import psycopg2
 
-# NEON PostgreSQL database URL
-DATABASE_URL = "INSERT NEON DATABASE URL HERE"
+# Establish connection using the connection string from config.py
+conn = psycopg2.connect(DATABASE_URL)
 
 # Create database engine
 engine = create_engine(DATABASE_URL)
 
 # Create tables
 Base.metadata.create_all(engine)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 print("Tables created successfully!")
